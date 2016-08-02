a5ea96e111f91dfe3529bade7581545878aed064
hanumuslem <hanumuslem@gmail.com>
2016-08-02 10:53:36 +0700

    mbenahin login api & web

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,38 +18,6 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 
-    public function login()
-    {
-        return view('login');
-    }
-
-    public function logout()
-    {
-        Auth::logout(); // log the user out of our application
-        return redirect('login');
-    }
-
-    public function login_process(Request $request){
-        $input = $request->all();
-        $userdata = array(
-            'username'     => $input['username'],
-            'password'  => $input['password']
-        );
-        if (Auth::attempt($userdata)) {
-
-            // validation successful!
-            // redirect them to the secure section or whatever
-            // return Redirect::to('secure');
-            // for now we'll just echo success (even though echoing in a controller is bad)
-            return redirect('dashboard');
-
-        } else {
-
-            // validation not successful, send back to form
-            return redirect('login');
-        }
-
-    }
 
 
     public function show()
